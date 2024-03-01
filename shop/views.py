71163de4cb2a5b5fd08444bca04752bef760239a
Thomas Rio <a.thomas.rio@gmail.com>
2024-02-29 22:32:10 -0500

    trying to get shop to work again

--- shop/views.py
+++ shop/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
-from .models import Category  # Add this line to import the Category model
-from django.shortcuts import get_object_or_404 # Add this line to import the get_object_or_404 function
+from .models import Product, Category
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
-
-class ProductListView(ListView):
-    model = Product
-    template_name = 'shop/index.html' # Use 'shop/index.html' for listing products
-    context_object_name = 'products'
-
-class ProductDetailView(DetailView): # This class-based view is for the product detail page
+class ProductDetailView(DetailView):
     model = Product
     template_name = 'shop/product_detail.html'
     context_object_name = 'product'
 
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shop/index.html'
+    context_object_name = 'products' #what is context_object_name? #context_object_name is the name of the variable that will be used in the template to represent the list of objects
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
 
+ 
+ 
 def index(request):
     return render(request, 'shop/index.html')
-
 
 def category_list(request): # This function gets all the categories that have no parent
     categories = Category.objects.filter(parent=None) # This line gets all the categories that have no parent
@@ -29,23 +30,13 @@ def category_detail(request, category_name):
     category = get_object_or_404(Category, name=category_name)
     return render(request, 'category_detail.html', {'category': category}) #the line 'category' is a key, this is a dictionary
 
-
 def sub_category_detail(request):
     return render(request, 'shop/sub_category_detail.html')
-
 
 def sub_category_detail(request, category_name, sub_category_name):
     sub_category = get_object_or_404(Category, name=sub_category_name, parent__name=category_name)
     products = Product.objects.filter(category=sub_category)
     return render(request, 'sub_category_detail.html', {'sub_category': sub_category, 'products': products})
 
+# Path: shop/models.py
 
-
-# how are the class views and the def views different?
-#   class views are used for more complex views, and def views are used for simpler views, such as the category_list and category_detail views
-
-# why dont the def views have a corresponding class in models.py? because the def views are simpler views, and do not require a model
-
-# what is a complex view vs a simple view?  a complex view is a view that requires more than one model, and a simple view is a view that requires only one model  
- 
- 
