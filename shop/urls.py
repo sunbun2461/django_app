d52ead9a0d5df8d1c6f1f73d6fc41702d1aa1706
Thomas Rio <a.thomas.rio@gmail.com>
2024-03-02 17:32:47 -0500

    attemping to make category page

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,11 @@
 # from .views import index, ProductListView, ProductDetailView
 from django.urls import path
-from .views import ProductListView, ProductDetailView, category_list, category_detail, sub_category_detail
+from .views import CategoryListView, ProductListView, ProductDetailView, category_list, category_detail, sub_category_detail
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),  # Main shop page
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Product detail page
-    path('category/', category_list, name='category_list'),
+    path('categories/', category_list, name='category_list'),
     path('c/<str:category_name>/', category_detail, name='category_detail'),
     path('c/<str:category_name>/<str:sub_category_name>/', sub_category_detail, name='sub_category_detail'),
 ]
