138cc5eac9d151dfeb398dbb7a3f4fbbaa504ca0
Thomas Rio <a.thomas.rio@gmail.com>
2024-03-02 20:21:47 -0500

    category list page works, awesome

--- shop/views.py
+++ shop/views.py
@@ -15,7 +15,7 @@ class ProductListView(ListView):
     
 class CategoryListView(ListView):
     model = Category
-    template_name = 'category_list.html'
+    template_name = 'shop/category_list.html'
     context_object_name = 'categories'
 
  
@@ -25,11 +25,11 @@ def index(request):
 
 def category_list(request): # This function gets all the categories that have no parent
     categories = Category.objects.filter(parent=None) # This line gets all the categories that have no parent
-    return render(request, 'category_list.html', {'categories': categories})#the line 'categories' is a key, this is a dictionary
+    return render(request, 'shop/category_list.html', {'categories': categories})#the line 'categories' is a key, this is a dictionary
 
 def category_detail(request, category_name):
     category = get_object_or_404(Category, name=category_name)
-    return render(request, 'category_detail.html', {'category': category}) #the line 'category' is a key, this is a dictionary
+    return render(request, 'shop/category_detail.html', {'category': category}) #the line 'category' is a key, this is a dictionary
 
 def sub_category_detail(request):
     return render(request, 'shop/sub_category_detail.html')
@@ -37,7 +37,7 @@ def sub_category_detail(request):
 def sub_category_detail(request, category_name, sub_category_name):
     sub_category = get_object_or_404(Category, name=sub_category_name, parent__name=category_name)
     products = Product.objects.filter(category=sub_category)
-    return render(request, 'sub_category_detail.html', {'sub_category': sub_category, 'products': products})
+    return render(request, 'shop/sub_category_detail.html', {'sub_category': sub_category, 'products': products})
 
 # Path: shop/models.py
 
